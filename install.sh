a9b6f69d4f4f7139adccf11a4d5344ec625a130d
Thomas Boerger <tboerger@tbpro.de>
2012-10-08 17:34:56 +0200

    Extended installer with rbenv gemset

--- install.sh
+++ install.sh
@@ -14,7 +14,8 @@ export RBENV_ROOT=$HOME/.rbenv
 export PATH="$RBENV_ROOT/shims:$RBENV_ROOT/bin:$PATH"
 
 curl https://raw.github.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash
-git clone https://github.com/joefiorini/rbenv-prompt.git $HOME/.rbenv/plugins/rbenv-prompt
+git clone https://github.com/joefiorini/rbenv-prompt.git $RBENV_ROOT/plugins/rbenv-prompt
+git clone https://github.com/jamis/rbenv-gemset.git $RBENV_ROOT/plugins/rbenv-gemset
 
 rbenv install 1.9.3-p194
 rbenv global 1.9.3-p194
